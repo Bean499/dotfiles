7553446ccb50ea07f2bb747cdd4fa43c97d9857c
Bean Robinson <bentr99@gmail.com>
2022-08-05 11:36:24 +0100

    dmenu now runs commands in kitty if they end in an exclamation mark

--- .scripts/misc/dmenu.sh
+++ .scripts/misc/dmenu.sh
@@ -3,4 +3,14 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+# dmenu_run improved
+# command ending with '!', is started in the terminal.
+
+test -s "$HOME"/.dmenurc && . "$HOME"/.dmenurc
+
+cmd="$(dmenu_path | dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" "$@")"
+
+case $cmd in
+    *\!) "${TERMINAL:-kitty}" -e "${cmd%?}" & ;;
+    *)   "${cmd}" & ;;
+esac
