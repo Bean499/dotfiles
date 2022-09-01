8065c6cb2e1ac7efd05c34c4374369fc88272603
Bean Robinson <bentr99@gmail.com>
2022-09-01 14:48:34 +0100

    menu scripts now use dmenu

--- .scripts/menus/music.sh
+++ .scripts/menus/music.sh
@@ -8,21 +8,26 @@ prev="   previous track"
 next="   next track"
 
 # Get answer from user via rofi
+# selected_option=$(echo "$playpause
+# $prev
+# $next
+# $what" | rofi -dmenu\
+#                   -i\
+#                   -p "music"\
+#                   -width "15"\
+#                   -lines 3\
+#                   -line-margin 3\
+#                   -line-padding 10\
+#                   -scrollbar-width "0" )
+
+# Get answer from user via rofi
+. "${HOME}/.cache/wal/colors.sh"
 selected_option=$(echo "$playpause
+$what
 $prev
-$next
-$what" | rofi -dmenu\
-                  -i\
-                  -p "music"\
-                  -width "15"\
-                  -lines 3\
-                  -line-margin 3\
-                  -line-padding 10\
-                  -scrollbar-width "0" )
+$next" | dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15")
 
 # Do something based on selected option
-
-
 if [ "$selected_option" == "$prev" ]
 then
     playerctl previous
--- .scripts/menus/screens.sh
+++ .scripts/menus/screens.sh
@@ -7,20 +7,25 @@ laptop="laptop"
 monitor="monitor"
 
 # Get answer from user via rofi
+# selected_option=$(echo "$both
+# $laptop
+# $monitor" | rofi -dmenu\
+#                   -i\
+#                   -p "screen layouts"\
+#                   -width "15"\
+#                   -lines 3\
+#                   -line-margin 3\
+#                   -line-padding 10\
+#                   -scrollbar-width "0" )
+
+# Get answer from user via dmenu
+. "${HOME}/.cache/wal/colors.sh"
 selected_option=$(echo "$both
 $laptop
-$monitor" | rofi -dmenu\
-                  -i\
-                  -p "screen layouts"\
-                  -width "15"\
-                  -lines 3\
-                  -line-margin 3\
-                  -line-padding 10\
-                  -scrollbar-width "0" )
+$monitor" | dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+)
 
 # Do something based on selected option
-
-
 if [ "$selected_option" == "$both" ]
 then
     ~/.scripts/screenlayouts/both.sh
