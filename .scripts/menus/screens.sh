f647a1c139b91ad59a0840e58f7785aee03175ad
Bean Robinson <bentr99@gmail.com>
2022-09-11 22:30:28 +0100

    updated screens menu to take parameter

--- .scripts/menus/screens.sh
+++ .scripts/menus/screens.sh
@@ -2,42 +2,36 @@
 
 # Options for powermenu
 
-both="monitor & laptop"
+both="both"
 laptop="laptop"
 monitor="monitor"
 
-# Get answer from user via rofi
-# selected_option=$(echo "$both
-# $laptop
-# $monitor" | rofi -dmenu\
-#                   -i\
-#                   -p "screen layouts"\
-#                   -width "15"\
-#                   -lines 3\
-#                   -line-margin 3\
-#                   -line-padding 10\
-#                   -scrollbar-width "0" )
-
-# Get answer from user via dmenu
-. "${HOME}/.cache/wal/colors.sh"
-selected_option=$(echo "$both
+if [ "$#" = "0" ]; then
+	# Get answer from user via dmenu
+	. "${HOME}/.cache/wal/colors.sh"
+	selected_option=$(echo "$both
 $laptop
 $monitor" | dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
-)
-
-# Do something based on selected option
-if [ "$selected_option" = "$both" ]
-then
-    ~/.scripts/screenlayouts/both.sh
-elif [ "$selected_option" = "$monitor" ]
-then
-    ~/.scripts/screenlayouts/monitor.sh
-elif [ "$selected_option" = "$laptop" ]
-then
-    ~/.scripts/screenlayouts/laptop.sh
+	)
+elif [ "$#" = "1" ]; then
+	selected_option=$1
 else
-    echo "No match"
+	echo "Too many arguments (expected 0 or 1)"
+	selected_option=""
 fi
 
-. ~/.cache/wal/colors.sh
-feh --bg-fill $wallpaper 
+# Do something based on selected option
+if [ "$selected_option" = "$both" ]; then
+    # ~/.scripts/screenlayouts/both.sh
+	xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-1 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI-1 --off
+	setxkbmap -layout us -option caps:escape
+elif [ "$selected_option" = "$monitor" ]; then
+    # ~/.scripts/screenlayouts/monitor.sh
+	xrandr --output eDP-1 --primary --off --output DP-1 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI-1 --off
+	setxbmap -layout us -option caps:escape
+elif [ "$selected_option" = "$laptop" ]; then
+	xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-1 --off --output HDMI-1 --off
+	setxkbmap -layout gb -option caps:escape
+fi
+
+~/.fehbg
